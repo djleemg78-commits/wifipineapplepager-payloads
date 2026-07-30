9bf2ae7bdf588f3b17b273cc97110a5e874122b0
Dallas Winger <9642419+dallaswinger@users.noreply.github.com>
2026-07-30 13:55:16 -0400

    Fix comment formatting in payload.sh

--- library/user/general/privacy-mode/payload.sh
+++ library/user/general/privacy-mode/payload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Title: toggle privacy mode
-#Description: Enables and disables privacy mode by edit the degub.json
-#Author: Rootjunky
-#Version: 2.0
+# Title: toggle privacy mode
+# Description: Enables and disables privacy mode by edit the degub.json
+# Author: Rootjunky
+# Version: 2.0
 
 FILE=/usr/debug.json
 
